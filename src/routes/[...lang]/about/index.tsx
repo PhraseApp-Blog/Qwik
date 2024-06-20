8ce0f46afd7fe5bc2dbec6458452127693f898e2
Mohammad Ashour <ashour.mohammad@gmail.com>
2024-06-20 19:39:38 +0100

    feat: extraction

--- src/routes/[...lang]/about/index.tsx
+++ src/routes/[...lang]/about/index.tsx
@@ -1,14 +1,15 @@
 import { component$ } from "@builder.io/qwik";
+import { inlineTranslate } from "qwik-speak";
 
 export default component$(() => {
+  const t = inlineTranslate();
+
   return (
     <article>
-      <h1 class="mb-2 text-2xl font-semibold">About us</h1>
-      <p>
-        Etttro is a retro hardware second-hand marketplace.
-        Buy and sell your retro gaming hardware, including
-        gaming consoles, computers, and accessories.
-      </p>
+      <h1 class="mb-2 text-2xl font-semibold">
+        {t("about.title")}
+      </h1>
+      <p>{t("about.content")}</p>
     </article>
   );
 });
